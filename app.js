c6d38908ca3ebcf959e10773b5452e4cf652c516
Conor Bailey <conorbailey@conors-mbp.home>
2019-08-25 15:16:10 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log('Website made by Conor Bailey (www.conbailey.com)')
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.navigation');
+const navLink = document.querySelectorAll(".nav-link");
+
+menuToggle.addEventListener('click', menTog);
+navLink.forEach(function(elem) {
+    elem.addEventListener("click", menTog);
+  });
+
+
+function menTog() {
+    menuToggle.classList.toggle("active");
+    navigation.classList.toggle("active");
+  }
+
+
+  // Smooth scroll script
+const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 800
+  });
