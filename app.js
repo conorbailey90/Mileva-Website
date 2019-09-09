a162d39dfcb20353f56b2ce830c422c7dac0b3c1
conorbailey90 <conbailey90@gmail.com>
2019-09-09 21:10:49 +0100

    Background and photo gallery added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.log('Website made by Conor Bailey (www.conbailey.com)')
 
+// Navigation Selectors / settings
 const menuToggle = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.navigation');
 const navLink = document.querySelectorAll(".nav-link");
@@ -24,6 +25,35 @@ function menTog() {
     navigation.classList.toggle("active");
   }
 
+
+// Press photo gallery settings
+
+const current = document.querySelector('#current');
+const opacity = 0.6;
+
+const imgs = document.querySelectorAll('.imgs img');
+
+//Set first image opacity
+
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e){
+  imgs.forEach(img => (img.style.opacity = 1 ));
+
+  current.src = e.target.src;
+
+  // Add fade in class
+
+  current.classList.add('fade-in');
+
+  // Remove fade in class after 0.5s
+
+  setTimeout(()=> current.classList.remove('fade-in'), 500);
+
+  e.target.style.opacity =opacity;
+}
 
   // Smooth scroll script
 const scroll = new SmoothScroll('a[href*="#"]', {
